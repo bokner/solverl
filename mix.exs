e2df0af3968fd4c2356a14d7b968420a22d60a7b
Boris Okner <boris.okner@gmail.com>
2020-07-02 20:42:42 -0400

    Add distillery dep

--- mix.exs
+++ mix.exs
@@ -23,6 +23,7 @@ defmodule Solverl.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+	{:distillery, "~> 2.1"}
     ]
   end
 end
