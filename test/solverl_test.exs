112118dfc574d8e00091cd257e39dd9ee4e9da5f
Boris Okner <boris.okner@gmail.com>
2020-07-14 17:07:26 -0400

    Add tests 1) for a sample solver run, 2) for checking computation of array dimensions.

--- test/solverl_test.exs
+++ test/solverl_test.exs
@@ -2,7 +2,28 @@ defmodule SolverlTest do
   use ExUnit.Case
   doctest Solverl
 
-  test "greets the world" do
-    assert Solverl.hello() == :world
+  
+  test "Runs the proper solver CMD" do
+    good_arr = [ [ [1,2,3], [2,3,1], [3,4,5] ], [ [1,2,3], [2,3,1], [3,4,5] ] ]
+    #assert MinizincData.dimensions(good_arr) == [2, 3, 3]
+    assert {:ok, _pid} = MinizincPort.start_link(
+             [
+               model: "mzn/small_all_different.mzn",
+               dzn: [
+                 %{
+                 test_data1: 100,
+                 test_arr: good_arr,
+                 test_base_arr: {[0, 1, 0], good_arr},
+                 test_set: MapSet.new([1,2,3]),
+                 test_enum: MapSet.new([:red, :blue, :white]) },
+                 "mzn/test_data2.dzn"],
+               solver: "gecode"])
+
   end
+
+  test "Checks dimensions of a regular array " do
+    good_arr = [ [ [1,2,3], [2,3,1], [3,4,5] ], [ [1,2,3], [2,3,1], [3,4,5] ] ]
+    assert MinizincData.dimensions(good_arr) == [2, 3, 3]
+  end
+
 end
