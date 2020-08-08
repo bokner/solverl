12e28b5e7753433ae0e4d4ad09d83f60743b8b40
Boris Okner <boris.okner@gmail.com>
2020-08-08 19:08:54 -0400

    Sync Erlang's 'minizinc' module with MinizincSolver

--- src/minizinc.erl
+++ src/minizinc.erl
@@ -12,9 +12,10 @@
 -define(MINIZINC, 'Elixir.MinizincSolver').
 
 %% API
--export([solve/1, solve/2, solve/3]).
--export([solve_sync/1, solve_sync/2, solve_sync/3]).
--export([stop_solver/1, get_solvers/0, get_solverids/0, lookup/1, get_executable/0]).
+-export([solve/1, solve/2, solve/3, solve/4]).
+-export([solve_sync/1, solve_sync/2, solve_sync/3, solve_sync/4]).
+-export([get_solvers/0, get_solverids/0, lookup/1]).
+-export([stop_solver/1, solver_status/1]).
 
 % Asynch solving
 solve(Model) ->
@@ -23,8 +24,11 @@ solve(Model) ->
 solve(Model, Data) ->
     solve(Model, Data, []).
 
-solve(Model, Data, Opts) ->
-    ?MINIZINC:solve(Model, Data, Opts).
+solve(Model, Data, SolverOpts) ->
+    ?MINIZINC:solve(Model, Data, SolverOpts).
+
+solve(Model, Data, SolverOpts, ServerOpts) ->
+    ?MINIZINC:solve(Model, Data, SolverOpts, ServerOpts).
 
 % Sync solving
 solve_sync(Model) ->
@@ -33,12 +37,17 @@ solve_sync(Model) ->
 solve_sync(Model, Data) ->
     solve_sync(Model, Data, []).
 
-solve_sync(Model, Data, Opts) ->
-    ?MINIZINC:solve_sync(Model, Data, Opts).
+solve_sync(Model, Data, SolverOpts) ->
+    ?MINIZINC:solve_sync(Model, Data, SolverOpts).
 
+solve_sync(Model, Data, SolverOpts, ServerOpts) ->
+    ?MINIZINC:solve_sync(Model, Data, SolverOpts, ServerOpts).
 
 stop_solver(Pid) ->
     ?MINIZINC:stop_solver(Pid).
+
+solver_status(Pid) ->
+    ?MINIZINC:solve_status(Pid).
 
 get_solvers() ->
     ?MINIZINC:get_solvers().
@@ -49,6 +58,4 @@ get_solverids() ->
 lookup(SolverId) when is_binary(SolverId) ->
     ?MINIZINC:lookup(SolverId).
 
-get_executable() ->
-    ?MINIZINC:get_executable().
 
