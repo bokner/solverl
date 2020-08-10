d95ee04a3a815b56f959fd2465e7b97e7117b889
Boris Okner <boris.okner@gmail.com>
2020-08-09 23:48:17 -0400

    Changes to MinizincInstance

--- lib/minizinc_instance.ex
+++ lib/minizinc_instance.ex
@@ -1,4 +1,30 @@
 defmodule MinizincInstance do
   @moduledoc false
 
+  def new(model, data \\ [], solver_opts \\ [], server_opts \\ []) do
+    %{
+      model: model,
+      data:  data,
+      solver_opts: solver_opts,
+      server_opts: server_opts
+    }
+  end
+
+  def run(instance) do
+    run(instance, true)
+  end
+
+  def run(%{
+      model: model,
+      data:  data,
+      solver_opts: solver_opts,
+      server_opts: server_opts
+    } = instance, sync) when is_map(instance) and is_boolean(sync) do
+
+    if sync do
+      MinizincSolver.solve_sync(model, data, solver_opts, server_opts)
+    else
+      MinizincSolver.solve(model, data, solver_opts, server_opts)
+    end
+  end
 end
