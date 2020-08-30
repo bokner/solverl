617177a101b797290035ed391ab89658cd860c9a
Boris Okner <boris.okner@gmail.com>
2020-08-29 22:00:51 -0400

    Version 1.0.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Solverl.MixProject do
   def project do
     [
       app: :solverl,
-      version: "1.0.3",
+      version: "1.0.4",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: ["lib", "examples"],
